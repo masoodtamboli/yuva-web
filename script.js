014610750a73bf000f0771a837c5ac2a7a327ff1
Masood Ismail Tamboli <mr.masoodtamboli@gmail.com>
2021-06-01 22:07:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function HospitalName() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("Input_name");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+
+    
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+function HospitalLocation() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("Input_location");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+
+    
+    td = tr[i].getElementsByTagName("td")[2];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
